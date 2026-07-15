5562fadc55986c012d65a92e2bda9c32db61ed73
Julia Emilly <juliaemillyrc@gmail.com>
2026-07-15 18:53:58 -0300

    feat: adiciona logo no ícone do celular

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.juliacarvalho.todolist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3"
+        versionCode = 5
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
